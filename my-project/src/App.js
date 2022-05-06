37ee463476672a6d36e634c329248187010c28f2
Dhamino <prasadraj1084@gmail.com>
2022-05-06 11:01:26 +0530

    cloned project

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-           hello world from vs code hello 
+           hello world from cloned vs code
       </header>
     </div>
   );
